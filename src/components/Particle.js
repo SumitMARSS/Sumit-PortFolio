f096db0d0ab834c062ddf9651c8c954807e272b1
SumitMARSS <sumit821044@gmail.com>
2024-08-17 14:02:56 +0530

    particle module change

--- src/components/Particle.js
+++ src/components/Particle.js
@@ -1,12 +1,20 @@
 
-import { useCallback } from "react";
-import Particles from "react-particles";
-import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
+import { useCallback, useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim"; 
+import React from "react";
+
 
 const Particle = () => {
-    const particlesInit = useCallback(async (engine) => {
-        console.log(engine);
-        await loadSlim(engine);
+    const [init, setInit] = useState(false);
+
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+            // Load the slim engine
+            await loadSlim(engine);
+        }).then(() => {
+            setInit(true);
+        });
     }, []);
 
     const particlesLoaded = useCallback(async (container) => {
@@ -14,88 +22,89 @@ const Particle = () => {
     }, []);
 
     return (
-        <Particles
-            id="tsparticles"
-            init={particlesInit}
-            loaded={particlesLoaded}
-            options={{
-                background: {
-                    color: {
-                        value: "transparent", 
+        init && (
+            <Particles
+                id="tsparticles"
+                particlesLoaded={particlesLoaded}
+                options={{
+                    background: {
+                        color: {
+                            value: "transparent", // Adjust this as needed
+                        },
                     },
-                },
-                fpsLimit: 150,
-                interactivity: {
-                    events: {
-                        onClick: {
-                            enable: true,
-                            mode: "push",
+                    fpsLimit: 150,
+                    interactivity: {
+                        events: {
+                            onClick: {
+                                enable: true,
+                                mode: "push",
+                            },
+                            onHover: {
+                                enable: true,
+                                mode: "repulse",
+                            },
+                            resize: true,
                         },
-                        onHover: {
-                            enable: true,
-                            mode: "repulse",
+                        modes: {
+                            push: {
+                                quantity: 4,
+                            },
+                            repulse: {
+                                distance: 100,
+                                duration: 0.4,
+                            },
                         },
-                        resize: true,
                     },
-                    modes: {
-                        push: {
-                            quantity: 4,
+                    particles: {
+                        color: {
+                            value: "#ffffff",
                         },
-                        repulse: {
+                        links: {
+                            color: "#ffffff",
                             distance: 100,
-                            duration: 0.4,
-                        },
-                    },
-                },
-                particles: {
-                    color: {
-                        value: "#ffffff", // Green color for particles
-                    },
-                    links: {
-                        color: "#ffffff",
-                        distance: 100,
-                        enable: true,
-                        opacity: 0.5,
-                        width: 0.7,
-                    },
-                    move: {
-                        direction: "none", // Random movement
-                        enable: true,
-                        outModes: {
-                            default: "bounce",
-                        },
-                        random: true,
-                        speed: 1,
-                        straight: false,
-                    },
-                    number: {
-                        density: {
                             enable: true,
-                            area: 800,
+                            opacity: 0.5,
+                            width: 0.7,
                         },
-                        value: 80,
+                        move: {
+                            direction: "none",
+                            enable: true,
+                            outModes: {
+                                default: "bounce",
+                            },
+                            random: true,
+                            speed: 1,
+                            straight: false,
+                        },
+                        number: {
+                            density: {
+                                enable: true,
+                                area: 800,
+                            },
+                            value: 100,
+                        },
+                        opacity: {
+                            value: 0.5,
+                        },
+                        shape: {
+                            type: "circle",
+                        },
+                        size: {
+                            value: { min: 0.8, max: 4 },
+                        },
                     },
-                    opacity: {
-                        value: 0.5,
-                    },
-                    shape: {
-                        type: "circle",
-                    },
-                    size: {
-                        value: { min: 0.8, max: 4 },
-                    },
-                },
-                detectRetina: true,
-            }}
-            style={{
-                position: "absolute", 
-                top: 0,
-                left: 0,
-                width: "100%",
-                height: "100%",
-                zIndex: 0, 
-            }}
-        />
+                    detectRetina: true,
+                }}
+                style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: "100%",
+                    zIndex: 0,
+                }}
+            />
+        )
     );
 };
 
